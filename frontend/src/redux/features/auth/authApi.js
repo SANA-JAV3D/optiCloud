5386500bdabb0cc226169c9b26df17250a1ddad6
Sana Javed <sj268266@gmail.com>
2025-04-26 00:42:01 +0530

    Initial commit

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -4,7 +4,7 @@ import { getBaseUrl } from "../../../utils/baseURL";
 const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${getBaseUrl()}/api/auth`,
+    baseUrl: `http://localhost:5000/api/auth`,
     credentials: "include",
   }),
   endpoints: (builder) => ({
@@ -22,43 +22,6 @@ const authApi = createApi({
         body: credentials,
       })
     })
-    // logoutUser: builder.mutation({
-    //   query: () => ({
-    //     url: "/logout",
-    //     method: "POST",
-    //   }),
-    // }),
-    // getUser: builder.query({
-    //   query: () => ({
-    //     url: "/users",
-    //     method: "GET",
-    //   }),
-    //   refetchOnMount: true,
-    //   invalidatesTags: ["User"],
-    // }),
-    // deleteUser: builder.mutation({
-    //     query: (userId) => ({
-    //         url: `/users/${userId}`,
-    //         method: "DELETE",
-    //     }),
-    //     invalidatesTags: ["User"],
-    // }),
-    // updateUerRole: builder.mutation({
-    //     query: ({userId, role}) => ({
-    //         url: `/users/${userId}`,
-    //         method: "PUT",
-    //         body: {role}
-    //       }),
-    //       refetchOnMount: true,
-    //       invalidatesTags: ["User"],
-    // }),
-    // editProfile: builder.mutation({
-    //     query: (profileData) => ({
-    //         url: `/edit-profile`,
-    //         method: "PATCH",
-    //         body: profileData
-    //       }),
-    // })
   })
 })
 
